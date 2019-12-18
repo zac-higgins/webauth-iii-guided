fd0d8e0ebcd6b9a48d350db24869c4f835f47bb9
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-18 13:24:07 -0700

    lecture follow along

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,14 +1,12 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
-
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken"); // installed this
+const Users = require("../users/users-model.js");
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
-
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -17,24 +15,36 @@ router.post('/register', (req, res) => {
       res.status(500).json(error);
     });
 });
-
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
-
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // sign token
+        const token = signToken(user); // new line
+        // send the token
         res.status(200).json({
+          token, // added token as part of the response sent
           message: `Welcome ${user.username}!`,
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
 });
-
+// this functions creates and signs the token
+function signToken(user) {
+  const payload = {
+    username: user.username,
+  };
+  const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
+  const options = {
+    expiresIn: "1h",
+  };
+  return jwt.sign(payload, secret, options); // notice the return
+}
 module.exports = router;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,18 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  const { authorization } = req.headers;
+  if (authorization) {
+    const secret = "is it secret, is it safe?";
+    jwt.verify(authorization, secret, function (err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: 'Invalid token.' });
+      } else {
+        req.token = decodedToken;
+        next();
+      }
+    });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: 'Please login and try again.' });
   }
 };
